b0e82bf2cec48510d6e717c0ab56dafb587f50bd
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-20 17:59:18 -0500

    database queries working

--- server.js
+++ server.js
@@ -19,9 +19,38 @@ const db = mysql.createConnection(
   console.log('Connection to election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
- console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//  console.log(rows);
+// });
+
+// get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// }); 
+
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// create candidte
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err,result) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log(result);
 });
+
 // default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
