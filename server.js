bb03577761689dc9f352f21e9d6c17d506a38f58
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-20 16:25:54 -0500

    sql bug

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'RockyRanch88',
+    database: 'election'
+  },
+  console.log('Connection to election database.')
+);
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
