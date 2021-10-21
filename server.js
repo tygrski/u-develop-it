50607998e249fde183610356475bcbdafc9d8761
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-21 15:35:40 -0500

    added join/table routes to API

--- server.js
+++ server.js
@@ -22,8 +22,12 @@ const db = mysql.createConnection(
 
 // get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
-
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
+  
   db.query(sql, (err, rows) => {
    if(err) {
      res.status(500).json({ error: err.message });
@@ -38,7 +42,12 @@ app.get('/api/candidates', (req, res) => {
 
 // get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
