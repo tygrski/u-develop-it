990a2b3520d792884d2865f37d273c5d6b9c1a87
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-22 17:15:39 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+router.use(require('./voterRoutes'));
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 
